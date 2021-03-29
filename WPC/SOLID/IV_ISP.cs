ba59eed84c8b76e1afec48ac7c0c3eabaf313cbd
Paweł Łęczycki <pawel@msd.info.pl>
2021-03-29 13:02:03 +0200

    ISP

---
+++ WPC/SOLID/IV_ISP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.SOLID.I
+{
+    interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+    interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+
+    class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+}
