b6746545cdf2e1c2279c2ec6f39d4ee7f8757967
Paweł Łęczycki <pawel@msd.info.pl>
2021-03-29 12:29:29 +0200

    OCP

---
+++ WPC/SOLID/II_OCP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.SOLID.O
+{
+    abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+
+    class Square : Shape
+    {
+        public int A { get; set; }
+
+        public override double Area => A * A;
+    }
+
+    class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public override double Area => A * B;
+    }
+
+    class Circle : Shape
+    {
+        public int R { get; set; }
+        public override double Area => Math.PI*R*R;
+    }
+
+    class ShapeCalculator
+    {
+        double Area(Shape shape)
+        {
+            return shape.Area;
+        }
+    }
+}
