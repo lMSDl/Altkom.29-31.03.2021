60ecf9373c21f812fdba6340282ac993f801005b
Paweł Łęczycki <pawel@msd.info.pl>
2021-03-31 09:22:19 +0200

    PrivateClassData

---
+++ WPC/DesignPatterns/StructuralPatterns/PrivateClassData/SomeClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.StructuralPatterns.PrivateClassData
+{
+    public class SomeClass
+    {
+        private SomeClassData _data;
+        private float _data3;
+
+        private readonly int _data4;
+        public int Data5 { get; }
+
+        public SomeClass(int data1, string data2, float data3)
+        {
+            _data = new SomeClassData(data1, data2);
+            _data3 = data3;
+
+            _data4 = 5;
+            Data5 = 6;
+        }
+
+        public string GetData2()
+        {
+            return _data.GetData2();
+        }
+
+        public int GetData1()
+        {
+            return _data.GetData1();
+        }
+
+        public void SomeAction()
+        {
+            _data3 = 3;
+        }
+    }
+}
---
+++ WPC/DesignPatterns/StructuralPatterns/PrivateClassData/SomeClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.StructuralPatterns.PrivateClassData
+{
+    public class SomeClassData
+    {
+        private int _data1;
+        private string _data2;
+
+        public SomeClassData(int data1, string data2)
+        {
+            _data1 = data1;
+            _data2 = data2;
+        }
+
+        public string GetData2()
+        {
+            return _data2;
+        }
+
+        public int GetData1()
+        {
+            return _data1;
+        }
+    }
+}
